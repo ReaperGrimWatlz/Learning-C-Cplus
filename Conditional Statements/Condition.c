79b599250b7fed708a2ac6399b71ce3258f91734
Reaper <149217292+ReaperGrimWatlz@users.noreply.github.com>
2024-07-30 12:04:29 +0000

    s

--- Conditional Statements/Condition.c
+++ Conditional Statements/Condition.c
@@ -2,15 +2,15 @@
 int main(){
     int a,b,sum;
     printf("Enter Numbers");
-    scanf('%d*%d',&a,&b);
+    scanf("%d%d",&a,&b);
     sum=a+b;
     printf("Sum of a&b is %d\n",sum);
     if (sum>20){
-        printf("Sum is greater than 20");
+        printf("Sum is greater than 20\n");
     }else if(sum>10){
-        printf("Sum is greater than 10");
+        printf("Sum is greater than 10\n");
     }else{
-        printf("Sum is less than 10");
+        printf("Sum is less than 10\n");
     }
     return 0;
 }
