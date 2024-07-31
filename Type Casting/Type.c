d2e72e636e55a7fb6b8921d5ec10205149605b21
Reaper <149217292+ReaperGrimWatlz@users.noreply.github.com>
2024-07-31 11:00:05 +0000

    Type Casting

---
+++ Type Casting/Type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    float c;
+    a=5;
+    b=2;
+    c=(float)a/b;
+    printf("Orignal value of C is %f\n",c);
+    printf(" Integer value of c is %d\n",(int)c);
+    return 0;
+}
---
+++ Type Casting/Type.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    float c;
+    a=5;
+    b=2;
+    c=(float)a/b;
+    cout<<"Orignal value of C is "<<c<<" Integer value of c is "<<(int)c<<endl;
+    return 0;
+}
