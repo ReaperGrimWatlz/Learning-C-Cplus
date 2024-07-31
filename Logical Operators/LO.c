c10d13fffde568eb0d24ba44573d2e29910f9ddf
Reaper <149217292+ReaperGrimWatlz@users.noreply.github.com>
2024-07-31 11:59:28 +0000

    wa# Please enter the commit message for your changes. Lines starting

--- Logical Operators/LO.c
+++ Logical Operators/LO.c
@@ -3,7 +3,7 @@ int main(){
     int a,b,c;
     printf("Enter the value of a ,b and c\n");
     scanf("%d %d %d",&a,&b,&c);
-    if((a>b && (a<c)))
+    if((a>b && (a>c)))
         printf("%d is greatest \n",a);
     else if (b>c)
         printf("%d is greatest \n",b);
--- Logical Operators/LO.cpp
+++ Logical Operators/LO.cpp
@@ -4,13 +4,13 @@ int main(){
     int a,b,c;
     cout<<"Enter the value of a ,b and c"<<endl;
     cin>>a>>b>>c;
-    if((a>b && (a<c)))
+    if((a>b && (a>c)))
         cout<<a<<" is greatest "<<endl;
     else if (b>c)
         cout<<b<<" is greatest "<<endl;
     else
     cout<<c<<" is greatest "<<endl;
     if ((a==0)||(b==0)||(c==0))
-    cout<<"Sum of"<<a<<b<<c<<"   is zero "<<endl;
+    cout<<"Sum of"<<a<<","<<b<<","<<c<<"   is zero "<<endl;
     return 0;
 }
