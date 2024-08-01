74073279661c2730554163677e3aee5c2df506fc
Reaper <149217292+ReaperGrimWatlz@users.noreply.github.com>
2024-08-01 09:26:47 +0000

    Why not

---
+++ 2)Intermediate Level/Arrays in c and c++/Array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int start[3]={4,5,6};
+    int suml;
+    suml=start[0]+start[1]+start[2];
+    printf("The sum is %d\n",suml);
+    return 0;
+}
---
+++ 2)Intermediate Level/Arrays in c and c++/Array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int start[3]={4,5,6};
+    int suml;
+    suml=start[0]+start[1]+start[2];
+    cout<<"The sum is "<<suml<<endl;
+    return 0;
+}
